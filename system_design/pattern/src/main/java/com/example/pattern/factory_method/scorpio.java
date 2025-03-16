39abd8b4f908a7a3899ba63759c449492b4919eb
sagar1998-dev <chauhanhunny623@gmail.com>
2025-03-16 13:42:33 +0530

    discard

--- system_design/pattern/src/main/java/com/example/pattern/factory_method/scorpio.java
+++
@@ -1,9 +0,0 @@
-package com.example.pattern;
-
-public class scorpio {
-    
-
-    public void drivecar(){
-        System.out.println(" Boom Boom ho rahi h ! ");
-    }
-}
--- system_design/pattern/src/main/java/com/example/pattern/factory_method/scorpioN.java
+++
@@ -1,9 +0,0 @@
-package com.example.pattern;
-
-public class scorpioN extends scorpio{
-  
-  
-    public void drivecar(){
-        System.out.println("scorpioN is driving");
-    }
-}
